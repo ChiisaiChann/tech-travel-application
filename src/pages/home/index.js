e4e093b2deeec14e5f504a6b81a536782d5ffc84
ChiisaiChann <mpaz.lilloc@gmail.com>
2022-05-24 23:10:16 -0400

    commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Subtitle, Title } from './styled';
+
+function Home() {
+  return (
+    <>
+      <Title>Home</Title>
+      <Subtitle>Hola Mundo</Subtitle>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/home/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  font-size: 120px;
+`;
+export const Subtitle = styled.p`
+  font-size: 50px;
+  color: green;
+`;
